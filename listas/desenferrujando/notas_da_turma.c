6337ac5aa83225f21cda9e0eba163c4bac54e853
Caio <caiofeliperro@gmail.com>
2023-09-08 21:12:08 -0300

    new problem solved

--- listas/desenferrujando/notas_da_turma.c
+++ listas/desenferrujando/notas_da_turma.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct celula {
     char matricula[10];
@@ -8,19 +9,20 @@ typedef struct celula {
 } celula;
 
 void relatorioMediaAtividades (celula *le, celula *l1, celula *l2, double media) {
-    int first1 = 0, first2 = 0, i = 0;
-
+    celula *l1v = l1, *l2v = l2;
     while(le->prox != NULL) {
+        le = le->prox;
+
         if(le->mediaAtividades <= media) {
-            l1->prox = le;
+            l1v->prox = le;
+            l1v = l1v->prox;
         }
         else {
-            l2->prox = le;
-        }
-        
-        le = le->prox;
+            l2v->prox = le;
+            l2v = l2v->prox;
+        }        
     }
 
-    l1->prox = NULL;
-    l2->prox = NULL;        
+    l1v->prox = NULL;
+    l2v->prox = NULL;        
 }
